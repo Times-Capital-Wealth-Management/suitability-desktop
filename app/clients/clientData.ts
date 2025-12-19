599f970821c93151e0bd9e523e0b68c3fe6e1462
Tarek <tareka357@outlook.com>
2025-12-19 21:49:40 +0000

    add client data

---
+++ app/clients/clientData.ts
@@ -0,0 +1,17 @@
+export const managers = ["Turon Miah"]
+
+export const salutations = ["Mr", "Ms", "Dr"]
+
+export const firstNames = ["Tarek",
+    "Shayek"]
+
+export const lastNames = ["Ahmed",
+    "Ahmed"]
+
+export const objectives = ["Capital Growth",
+    "Balanced"]
+
+export const risks = ["Medium",
+    "High"]
+
+export const capacityOfLoss = [20, 50]
